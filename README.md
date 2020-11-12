23b67fa02799e484b4b4600ce84d5e27b5611933
Tomas <tcaraccia@interviewstream.com>
2020-11-12 16:38:55 -0300

    Readme.md

---
+++ README.md
@@ -0,0 +1,12 @@
+#Install
+- Genero usuario admin
+- Seteo Api en Tabla
+
+
+## Clients
+- ApiKey
+
+## Users
+- 1 Client
+
+## Flags
---
+++ migrations/1604745934772_sample_flags/up.sql
@@ -0,0 +1,3 @@
+INSERT INTO flags (uid, enabled, description, label) VALUES
+('my_flag1', true, 'My Flag 1', 'some flag'),
+('my_flag2', false, 'My Flag 2', 'some flag 2');
